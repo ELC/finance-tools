a9366cb00a40258e2af240042068f85ad3907083
ELC <elcg@gmx.com>
2022-01-09 03:03:58 -0300

    Add Inflation Simulator

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,7 +1,9 @@
 from .compound_interest import compound_interest
 from .flexfixed import flexfixed
+from .inflation import inflation_simulation
 
 pages = {
     "Compound Interest": compound_interest,
     "Flex Term vs Fixed Term": flexfixed,
+    "Inflation Simulation": inflation_simulation,
 }
